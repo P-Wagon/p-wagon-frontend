1903d70c5bd8d07668ec1389715e78935fdd1513
thehyperpineapple <raghav.sampath2003@gmail.com>
2024-03-15 17:27:12 +0530

    fix: Spotting attribute

--- components/dataTable.tsx
+++ components/dataTable.tsx
@@ -7,10 +7,19 @@ interface CrimeDetails {
   license: string;
 }
 
+interface Spotting {
+  license: string;
+  location: string;
+  timeStamp: string;
+}
+
 interface CrimeRecord {
   description: string;
   details: CrimeDetails;
   estimatedTime: string;
+  spottings: false | {
+    [key: string]: Spotting;
+  };
   status: boolean;
 }
 
@@ -21,46 +30,70 @@ interface Crimes {
 const CrimesTable: React.FC = () => {
   const [crimes, setCrimes] = useState<CrimeRecord[]>([]);
 
+  const [loading, setLoading] = useState(false);
+
+
+
   useEffect(() => {
     const fetchCrimes = async () => {
       try {
+        setLoading(true)
         const response = await axios.get<Crimes>('https://p-wagon-backend.vercel.app/api/fetchCrimes');
-        // Convert the object of objects into an array of objects
         const crimesArray = Object.values(response.data);
         setCrimes(crimesArray);
+        setLoading(false)
       } catch (error) {
         console.error('There was an error fetching the crimes:', error);
       }
     };
-    
+
     fetchCrimes();
   }, []);
 
   return (
-    <TableContainer>
-      <Table variant='simple'>
-        <Thead>
-          <Tr>
-            <Th>Description</Th>
-            <Th>License Plate</Th>
-            <Th>Color</Th>
-            <Th>Estimated Time</Th>
-            <Th>Status</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {crimes.map((crime, index) => (
-            <Tr key={index}>
-              <Td>{crime.description}</Td>
-              <Td>{crime.details.license}</Td>
-              <Td>{crime.details.color}</Td>
-              <Td>{crime.estimatedTime}</Td>
-              <Td>{crime.status ? 'Active' : 'Inactive'}</Td>
+    <>
+    { loading ? "loading..." : 
+         <TableContainer>
+        <Table variant='simple'>
+            <Thead>
+            <Tr>
+                <Th>Description</Th>
+                <Th>License Plate</Th>
+                <Th>Color</Th>
+                <Th>Estimated Time</Th>
+                <Th>Spottings</Th>
+                <Th>Status</Th>
             </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </TableContainer>
+            </Thead>
+            <Tbody>
+            {crimes.map((crime, index) => (
+                <Tr key={index}>
+                <Td>{crime.description}</Td>
+                <Td>{crime.details.license}</Td>
+                <Td>{crime.details.color}</Td>
+                <Td>{crime.estimatedTime}</Td>
+                <Td>
+                    {crime.spottings ? (
+                    <ul>
+                        {Object.values(crime.spottings).map((spotting, index) => (
+                        <li key={index}>
+                            {spotting.location} - {spotting.timeStamp}
+                        </li>
+                        ))}
+                    </ul>
+                    ) : (
+                    'N/A'
+                    )}
+                </Td>
+                <Td>{crime.status ? 'Active' : 'Inactive'}</Td>
+                </Tr>
+            ))}
+            </Tbody>
+        </Table>
+        </TableContainer>
+        }
+    </>
+   
   );
 };
 
